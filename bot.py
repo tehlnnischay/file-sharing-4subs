85b31b8a8acca45e99d2f9fccd08d48e9df9ff4e
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-05-17 06:27:39 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -100,9 +100,12 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Madflix_Bots")
-        self.LOGGER(__name__).info(f"""
-── Bot Running..!\n\nCreated by \nhttps://t.me/ultroid_official""")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/ultroid_official")
+        self.LOGGER(__name__).info(f""" \n\n 
+        bot running...!
+(っ◔◡◔)っ ♥ ULTROIDOFFICIAL ♥
+░╚════╝░░╚════╝░╚═════╝░╚══════╝
+                                         """)
         self.username = usr_bot_me.username
         #web-response
         app = web.AppRunner(await web_server())
