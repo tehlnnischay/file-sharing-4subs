514de5d6cee038d88782197c1003af9e87f18014
tehlnnischay <tehlannischay0001@gmail.com>
2024-05-17 20:17:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,22 +9,22 @@ from logging.handlers import RotatingFileHandler
 
 
 
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-APP_ID = int(os.environ.get("APP_ID", "22505271"))
-API_HASH = os.environ.get("API_HASH", "c89a94fcfda4bc06524d0903977fc81e")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7068442783:AAH9zdA33dFiOI5ZUnMLCxlqxXaX0jhX8Vc")
+APP_ID = int(os.environ.get("APP_ID", "27315697"))
+API_HASH = os.environ.get("API_HASH", "dbc404a4c78480c09dd5d693fc13c1b8")
 
 
-OWNER = os.environ.get("OWNER", "PlayTime_Jatin") #Owner username
-OWNER_ID = int(os.environ.get("OWNER_ID", "6695586027")) #Owner user id
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://ultroidxTeam:ultroidxTeam@cluster0.gabxs6m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DB_NAME", "uxbbot")
+OWNER = os.environ.get("OWNER", "hellloska") #Owner username
+OWNER_ID = int(os.environ.get("OWNER_ID", "6820525129")) #Owner user id
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://thor:7thor7@thor.aymqpdl.mongodb.net/?retryWrites=true&w=majority&appName=thor")
+DB_NAME = os.environ.get("DB_NAME", "HDMEDIA03")
 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002075726565")) #database id 
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002108419450"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1001976541518"))
-FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "-1001982072622"))
-FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "-1002072891392"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002141441410")) #database id 
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001935247305"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002113909424"))
+FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "-1002067058871"))
+FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "-1002144760143"))
 
 
 SECONDS = int(os.getenv("SECONDS", "200")) # auto delete in seconds
@@ -40,8 +40,8 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 
 try:
-    ADMINS=[6020516635]
-    for x in (os.environ.get("ADMINS", "6020516635 6695586027").split()):
+    ADMINS=[5146039165]
+    for x in (os.environ.get("ADMINS", "5146039165").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -59,7 +59,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot !"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(6695586027)
+ADMINS.append(6820525129)
 
 LOG_FILE_NAME = "uxblogs.txt"
 
